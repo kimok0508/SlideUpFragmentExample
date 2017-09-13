b47ffa8c25598c36afe6858e3b4d29775154a9ee
kimok0508 <kimok0508@naver.com>
2017-09-13 23:15:44 +0900

    frament register added

---
+++ app/src/main/java/kr/edcan/slideupfragmentexample/RegisterFragment.java
@@ -0,0 +1,31 @@
+package kr.edcan.slideupfragmentexample;
+
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import kr.edcan.slideupfragmentexample.databinding.FragmentRegisterBinding;
+
+/**
+ * Created by mac on 2017. 9. 13..
+ */
+
+public class RegisterFragment extends Fragment {
+    private FragmentRegisterBinding binding;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_register, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
