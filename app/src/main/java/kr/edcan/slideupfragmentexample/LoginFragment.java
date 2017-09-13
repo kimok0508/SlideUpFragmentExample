34a4552d93b897766fb97b9a3ef5355f7fd0763b
kimok0508 <kimok0508@naver.com>
2017-09-13 23:09:50 +0900

    frament login added

---
+++ app/src/main/java/kr/edcan/slideupfragmentexample/LoginFragment.java
@@ -0,0 +1,31 @@
+package kr.edcan.slideupfragmentexample;
+
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import kr.edcan.slideupfragmentexample.databinding.FragmentLoginBinding;
+
+/**
+ * Created by mac on 2017. 9. 13..
+ */
+
+public class LoginFragment extends Fragment {
+    private FragmentLoginBinding binding;
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_login, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
